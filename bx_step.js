c014d206da64fe14b52c7335ab4a2461a79e5def
root <root@eylu.(none)>
2015-09-28 09:53:13 +0800

    添加bx_step.js，并添加了单元测试

---
+++ bx_step.js
@@ -0,0 +1,146 @@
+"use strict";
+
+var BxStep = (function () {
+	//构造函数
+	//elementsArr，代表各个层对象，
+	var bxStep = function (elementsArr)
+	{
+		this.elements = elementsArr; 
+	};
+
+	//显示指定的对象层，如果没有找到，会抛出Error异常
+	bxStep.prototype.showIndex = function(index)
+	{
+		if (this.elements[index])
+		{
+			this.elements[index].style.display = "";
+		}
+		else
+		{
+			throw new Error("没有那个元素");
+		}
+
+		//如果index存在，隐藏其他的元素
+		for (var i = 0; i < this.elements.length; i++)
+		{
+			if (i != index && this.elements[i])
+			{
+				this.elements[i].style.display = "none";
+			}
+		}
+	};
+
+	//隐藏指定层
+	bxStep.prototype.hideIndex = function(index)
+	{
+		if (this.elements[index])
+		{
+			this.elements[index].style.display = "none";
+		}
+		else
+		{
+			throw new Error("没有那个元素");
+		}
+	};
+
+	//获取当前显示的层对象
+	bxStep.prototype.getShowElement = function()
+	{
+		for (var i = 0; i < this.elements.length; i++)
+		{
+			if (this.elements[i] && this.elements[i].style.display != "none")
+			{
+				return this.elements[i];
+			}
+		}
+
+		return null;
+	};
+
+	//获取当前显示层索引
+	bxStep.prototype.getShowIndex = function()
+	{
+		for (var i = 0; i < this.elements.length; i++)
+		{
+			if (this.elements[i] && this.elements[i].style.display != "none")
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	};
+
+	//隐藏全部的层
+	bxStep.prototype.hideAll = function()
+	{
+		for (var i = 0; i < this.elements.length; i++)
+		{
+			if (this.elements[i])
+			{
+				this.elements[i].style.display = "none";
+			}
+		}
+	};
+
+	//添加一个层对象
+	bxStep.prototype.add = function(element, index)
+	{
+		if (isNaN(index))
+		{
+			this.elements.push(element);
+		}
+		else if (index >= 0 && index <= this.elements.length)
+		{
+			this.elements.splice(index, 0, element);
+		}
+		else
+		{
+			throw new Error("index如果提供值，必须大于等于0且不能大于数组长度");
+		}
+	};
+
+	//删除一个指定索引的层对象
+	bxStep.prototype.removeIndex = function(index)
+	{
+		if (this.elements[index])
+		{
+			this.elements.splice(index, 1);
+		}
+		else
+		{
+			throw new Error("指定索引不存在");
+		}
+	};
+
+	//删除一个层对象
+	//element为null，抛出ReferenceError异常，如果element不存在，抛出Error异常
+	bxStep.prototype.removeElement = function(element)
+	{
+		if (element)
+		{
+			var isDelete = false;
+
+			for (var i = 0; i < this.elements.length; i++)
+			{
+				if (this.elements[i] == element)
+				{
+					isDelete = true;
+					this.elements.splice(i, 1);
+				}
+			}
+			if (!isDelete)
+			{
+				throw new Error("并不存在element对应的层对象");
+			}
+		}
+		else
+		{
+			throw new ReferenceError("element不能为null");
+		}
+	};
+
+	//替换对象，放到下一版
+
+	return bxStep;
+})();
